d8b84fbe51f241e20e64e9a7074ad965aef55c64
Simon Lepasteur <simon.lepasteur@swissdotnet.ch>
2018-04-27 15:08:55 +0200

    Add immer library path.

--- include/overloaded.h
+++ include/overloaded.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundefined-internal"
+#pragma clang diagnostic ignored "-Wundefined-func-template"
+
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
+#pragma clang diagnostic pop
